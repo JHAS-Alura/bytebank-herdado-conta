852f0cb9a11a592862b7b561c992cd8508d095fe
João Henrique <Joao@Mac-mini-de-ahazou.local>
2020-06-17 22:59:52 -0300

    aulas 1 e 2 - Java parte 6: Conhecendo o java.util

---
+++ src/br/com/bytebank/banco/test/TestArrayStrings.java
@@ -0,0 +1,17 @@
+package br.com.bytebank.banco.test;
+
+
+public class TestArrayStrings {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Funcionou !!");
+		
+		//verificar Run Configurations para ver os argumentos passados
+		for(int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+		
+	}
+
+}
